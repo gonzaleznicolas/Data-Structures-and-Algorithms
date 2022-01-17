905fbf0a0406bc7ed77c44c6030cef5e03376f03
Nicolas Gonzalez <nicolas.gonzalez@arcurve.ca>
2022-01-16 21:44:23 -0700

    iterative binary search

---
+++ binarySearch/iterative/binarySearch.ts
@@ -0,0 +1,26 @@
+let sortedArray = [1,2,3,4,5];
+let valueToFind = 4;
+let index = binarySearch(sortedArray, valueToFind);
+console.log(`In the array ${sortedArray} the index of the value ${valueToFind} is ${index}`);
+
+// precondition: ary is sorted with no repeated values
+// returns: index of value, or -1 if value not in sortedAry
+function binarySearch(ary: number[], value: number) {
+    let startIndex = 0; // inclusive
+    let endIndex = ary.length - 1; // inclusive
+    let valueIndex = -1;
+    while (startIndex <= endIndex) {
+        let midIndex = Math.floor((startIndex + endIndex) / 2);
+
+        if (ary[midIndex] === value) {
+            valueIndex = midIndex;
+            break;
+        } else if (ary[midIndex] < value) {
+            startIndex = midIndex + 1;
+        } else { // value < ary[midIndex]
+            endIndex = midIndex - 1;
+        }
+    }
+
+    return valueIndex
+}
