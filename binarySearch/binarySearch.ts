c1e04992d22b07f85011c4dd39441c6748a50b9d
Nicolas Gonzalez <nicolas.gonzalez@arcurve.ca>
2022-01-16 20:57:46 -0700

    fix

--- binarySearch/binarySearch.ts
+++ binarySearch/binarySearch.ts
@@ -1,9 +1,10 @@
 let sortedArray = [1,2,3,4,5];
-let valueToFind = 5;
+let valueToFind = 0;
 let index = binarySearch(sortedArray, valueToFind);
 console.log(`In the array ${sortedArray} the index of the value ${valueToFind} is ${index}`);
 
-// returns index of first instance of value, or -1 if value not in sortedAry
+// precondition: sortedAry is sorted with no repeated values
+// returns: index of value, or -1 if value not in sortedAry
 function binarySearch(sortedAry: number[], value: number) {
     return bsHelper(sortedAry, value, 0, sortedAry.length - 1)
 }
@@ -15,15 +16,13 @@ function bsHelper(ary: number[], value: number, startIndex: number, endIndex: nu
         return -1;
     }
 
-    if (startIndex === endIndex) {
-        return ary[startIndex] === value ? startIndex : -1;
-    }
-
     let midIndex = Math.floor((startIndex + endIndex) / 2);
-    let leftResult = bsHelper(ary, value, startIndex, midIndex);
-    if (leftResult !== -1) {
-        return leftResult;
-    }
 
-    return bsHelper(ary, value, midIndex + 1, endIndex);
+    if (ary[midIndex] === value) {
+        return midIndex;
+    } else if (value < ary[midIndex]) {
+        return bsHelper(ary, value, startIndex, midIndex - 1);
+    } else { // ary[midIndex] < value
+        return bsHelper(ary, value, midIndex + 1, endIndex)
+    }
 }
