5edc497c1979a042cd4a539220aa89bfa47b4a80
Nicolas Gonzalez <nicolas.gonzalez@arcurve.ca>
2022-01-17 20:42:29 -0700

    wip

--- mergeSort/mergeSort.ts
+++ mergeSort/mergeSort.ts
@@ -1,24 +1,45 @@
 let aryToSort = [5, 6, 7, 2, 7, 4, 8];
+console.log(mergeSort(aryToSort).join(","));
 
-let ary1 = [];
-let ary2 = [1];
+function mergeSort(ary: number[]): number[] {
+    if (ary.length === 1) {
+        return ary;
+    }
 
-console.log(merge(ary1, ary2).join(','));
+    let firstIndexOfRight = Math.floor(ary.length / 2);
+    let left = ary.slice(0, firstIndexOfRight);
+    let right = ary.slice(firstIndexOfRight, ary.length + 1);
+    
+    return merge(mergeSort(left), mergeSort(right));
+}
 
 // precondition: a and b are sorted
 function merge(a: number[], b: number[]): number[] {
-    let mergedAry = Array(a.length + b.length);
+    let mergedAry = new Array(a.length + b.length);
     let ia = 0;
     let ib = 0;
     let im = 0;
-    while (ia <= a.length && ib <= b.length) {
-        if ((ia < a.length && a[ia] <= b[ib]) || b.length <= ib) {
+
+    while (ia < a.length && ib < b.length) {
+        if (a[ia] <= b[ib]) {
             mergedAry[im] = a[ia];
             ia++;
         } else {
             mergedAry[im] = b[ib];
             ib++;
         }
+        im++;
+    }
+
+    while (ia < a.length) {
+        mergedAry[im] = a[ia];
+        ia++;
+        im++;
+    }
+
+    while (ib < b.length) {
+        mergedAry[im] = b[ib];
+        ib++;
         im++;
     }
 
