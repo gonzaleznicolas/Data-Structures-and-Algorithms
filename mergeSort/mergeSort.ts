3cec2baa482ce75537f649d724f3c66ce1316e4c
Nicolas Gonzalez <nicolas.gonzalez@arcurve.ca>
2022-01-17 22:18:56 -0700

    wip

--- mergeSort/mergeSort.ts
+++ mergeSort/mergeSort.ts
@@ -1,47 +1,56 @@
-let aryToSort = [5, 6, 7, 2, 7, 4, 8];
-console.log(mergeSort(aryToSort).join(","));
+let aryToSort = [8, 7, 6, 5, 4, 3, 2];
+mergeSort(aryToSort);
+console.log(aryToSort.join(","));
 
-function mergeSort(ary: number[]): number[] {
-    if (ary.length === 1) {
-        return ary;
-    }
-
-    let firstIndexOfRight = Math.floor(ary.length / 2);
-    let left = ary.slice(0, firstIndexOfRight);
-    let right = ary.slice(firstIndexOfRight, ary.length + 1);
-    
-    return merge(mergeSort(left), mergeSort(right));
+function mergeSort(ary: number[]) {
+    let space = new Array<number>(ary.length);
+    mergeSortHelper(ary, space, 0, ary.length);
 }
 
-// precondition: a and b are sorted
-function merge(a: number[], b: number[]): number[] {
-    let mergedAry = new Array(a.length + b.length);
-    let ia = 0;
-    let ib = 0;
-    let im = 0;
+// start is inclusive, end is exclusive
+function mergeSortHelper(ary: number[], space: number[], start: number, end: number) {
+    if (end <= start + 1) {
+        return;
+    }
 
-    while (ia < a.length && ib < b.length) {
-        if (a[ia] <= b[ib]) {
-            mergedAry[im] = a[ia];
-            ia++;
+    let mid = Math.floor((start + end) / 2);
+
+    mergeSortHelper(ary, space, start, mid); // sort left
+    mergeSortHelper(ary, space, mid, end); // sort right
+    merge(ary, space, start, mid, end)
+}
+
+// precondition: start - mid(exclusive), and mid - end(exclusive) are sorted
+// postcondition: merged sorted arrays in start - end(exclusive)
+function merge(ary: number[], space: number[], start: number, mid: number, end: number) {
+    // copy start-end into the space
+    for (let i = start; i < end; i++) {
+        space[i] = ary[i];
+    }
+
+    let il = start; // index left
+    let ir = mid; // index right
+    let im = start; // index merged
+    while (il < mid && ir < end) {
+        if (space[il] <= space[ir]) {
+            ary[im] = space[il];
+            il++;
         } else {
-            mergedAry[im] = b[ib];
-            ib++;
+            ary[im] = space[ir];
+            ir++;
         }
         im++;
     }
 
-    while (ia < a.length) {
-        mergedAry[im] = a[ia];
-        ia++;
+    while (il < mid) {
+        ary[im] = space[il];
+        il++;
         im++;
     }
 
-    while (ib < b.length) {
-        mergedAry[im] = b[ib];
-        ib++;
+    while (ir < end) {
+        ary[im] = space[ir];
+        ir++;
         im++;
     }
-
-    return mergedAry;
 }
