bd9f6bc67aa46971cfc00960bb70a0358dffb229
Nicolas Gonzalez <nicolas.gonzalez@arcurve.ca>
2022-01-17 21:21:41 -0700

    make end index exclusive

--- binarySearch/recursive/binarySearch.ts
+++ binarySearch/recursive/binarySearch.ts
@@ -1,18 +1,19 @@
 let sortedArray = [1,2,3,4,5];
-let valueToFind = 0;
+let valueToFind = 4;
 let index = binarySearch(sortedArray, valueToFind);
 console.log(`In the array ${sortedArray} the index of the value ${valueToFind} is ${index}`);
 
 // precondition: sortedAry is sorted with no repeated values
 // returns: index of value, or -1 if value not in sortedAry
 function binarySearch(sortedAry: number[], value: number) {
-    return bsHelper(sortedAry, value, 0, sortedAry.length - 1)
+    return bsHelper(sortedAry, value, 0, sortedAry.length)
 }
 
-// startIndex and endIndex are inclusive
+// startIndex is inclusive
+// endIndex is exclusive
 // ary should be sorted
 function bsHelper(ary: number[], value: number, startIndex: number, endIndex: number) {
-    if (endIndex < startIndex) { // subarray is empty
+    if (endIndex <= startIndex) { // subarray is empty
         return -1;
     }
 
@@ -21,8 +22,8 @@ function bsHelper(ary: number[], value: number, startIndex: number, endIndex: nu
     if (ary[midIndex] === value) {
         return midIndex;
     } else if (value < ary[midIndex]) {
-        return bsHelper(ary, value, startIndex, midIndex - 1);
+        return bsHelper(ary, value, startIndex, midIndex);
     } else { // ary[midIndex] < value
-        return bsHelper(ary, value, midIndex + 1, endIndex)
+        return bsHelper(ary, value, midIndex, endIndex)
     }
 }
