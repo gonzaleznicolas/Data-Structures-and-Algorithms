c5772ffb7a8a6797f84baf149ca7da77d72f85c5
Nicolas Gonzalez <nicolas.gonzalez@arcurve.ca>
2022-04-23 22:03:24 -0600

    removed the LinkedList class

--- hashTable/chaining/Program.cs
+++ hashTable/chaining/Program.cs
@@ -39,6 +39,7 @@ public class Program
 		Console.WriteLine(dx);
 
 		Console.WriteLine(d.Remove("Nico"));
+		Console.WriteLine(d.Remove("Nico"));
 		Console.WriteLine(d.Remove("Nico7"));
 	}
 }
@@ -46,134 +47,106 @@ public class Program
 
 public class Dictionary<TKey, TValue> where TKey : IEquatable<TKey>
 {
-	private LinkedList[] LinkedLists { get; set; }
+	private Node[] LinkedLists { get; set; }
 
 	public Dictionary(int initialCapacity = 10)
 	{
-		LinkedLists = new LinkedList[initialCapacity];
-		for (int i = 0; i < LinkedLists.Length; i++) LinkedLists[i] = new LinkedList();
+		LinkedLists = new Node[initialCapacity];
 	}
 
 	public bool Insert(TKey key, TValue value)
 	{
-		var ll = LinkedLists[KeyToIndex(key)];
-		return ll.Insert(key, value);
+		var index = GetIndex(key);
+		var newNode = new Node(key, value);
+		if (LinkedLists[index] == null)
+		{
+			LinkedLists[index] = newNode;
+		}
+		else
+		{
+			// The list is not empty, iterate through to make sure the key does not already exist
+			Node pointer = LinkedLists[index];
+			while (pointer != null)
+			{
+				if (pointer.Key.Equals(newNode.Key))
+				{
+					return false;
+				}
+				else
+				{
+					if (pointer.Next == null) {
+						pointer.Next = newNode;
+						pointer = null;
+					}
+					else
+					{
+						pointer = pointer.Next;
+					}
+				}
+			}
+		}
+		return true;
 	}
 
 	public bool GetByKey(TKey key, out TValue value)
 	{
-		var ll = LinkedLists[KeyToIndex(key)];
-		return ll.GetByKey(key, out value);
+		var index = GetIndex(key);
+		Node pointer = LinkedLists[index];
+		while (pointer != null) {
+			if (pointer.Key.Equals(key)) {
+				value = pointer.Value;
+				return true;
+			} else {
+				pointer = pointer.Next;
+			}
+		}
+		value = default(TValue);
+		return false;
 	}
 
 	public bool Remove(TKey key)
 	{
-		var ll = LinkedLists[KeyToIndex(key)];
-		return ll.Remove(key);
+		var index = GetIndex(key);
+		if (LinkedLists[index] == null) {
+			return false;
+		} else if (LinkedLists[index].Key.Equals(key)) {
+			LinkedLists[index] = LinkedLists[index].Next;
+			return true;
+		} else {
+			// The list is not empty and key is not at the head
+			Node pointer = LinkedLists[index];
+			while (pointer.Next != null)
+			{
+				if (pointer.Next.Key.Equals(key))
+				{
+					pointer.Next = pointer.Next.Next;
+					return true;
+				} else
+				{
+					pointer = pointer.Next;
+				}
+			}
+			return false;
+		}
 	}
 
-	private int KeyToIndex(TKey key)
+	private int GetIndex(TKey key)
 	{
 		var hash = key.GetHashCode();
 		var positive = hash & 0x7FFFFFFF;
 		return positive % LinkedLists.Length;
 	}
 
-	public class LinkedList
+	private class Node
 	{
-		private Node Head { get; set; }
+		public TKey Key { get; set; }
+		public TValue Value { get; set; }
+		public Node Next { get; set; }
 
-		public bool IsEmpty {
-			get
-			{
-				return Head == null;
-			}
-		}
-
-		public bool Insert(TKey key, TValue value)
+		public Node(TKey key, TValue value)
 		{
-			var newNode = new Node(key, value);
-			if (Head == null)
-			{
-				Head = newNode;
-			}
-			else
-			{
-				// The list is not empty, iterate through to make sure the key does not already exist
-				Node pointer = Head;
-				while (pointer != null)
-				{
-					if (pointer.Key.Equals(newNode.Key))
-					{
-						return false;
-					}
-					else
-					{
-						if (pointer.Next == null) {
-							pointer.Next = newNode;
-							pointer = null;
-						}
-						else
-						{
-							pointer = pointer.Next;
-						}
-					}
-				}
-			}
-			return true;
-		}
-
-		public bool GetByKey(TKey key, out TValue value)
-		{
-			Node pointer = Head;
-			while (pointer != null) {
-				if (pointer.Key.Equals(key)) {
-					value = pointer.Value;
-					return true;
-				} else {
-					pointer = pointer.Next;
-				}
-			}
-			value = default(TValue);
-			return false;
-		}
-
-		public bool Remove(TKey key)
-		{
-			if (Head == null) {
-				return false;
-			} else if (Head.Key.Equals(key)) {
-				Head = Head.Next;
-				return true;
-			} else {
-				// The list is not empty and key is not at the head
-				Node pointer = Head;
-				while (pointer.Next != null)
-				{
-					if (pointer.Next.Key.Equals(key))
-					{
-						pointer.Next = pointer.Next.Next;
-						return true;
-					} else
-					{
-						pointer = pointer.Next;
-					}
-				}
-				return false;
-			}
-		}
-
-		private class Node
-		{
-			public TKey Key { get; set; }
-			public TValue Value { get; set; }
-			public Node Next { get; set; }
-
-			public Node(TKey key, TValue value)
-			{
-				Key = key;
-				Value = value;
-			}
+			Key = key;
+			Value = value;
 		}
 	}
 }
