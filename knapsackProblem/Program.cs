0612554d4be92f0605cd32c4f57924056712974c
Nicolas Gonzalez <nicolas.gonzalez@arcurve.ca>
2022-05-29 09:39:51 -0600

    solved knapsack problem

---
+++ knapsackProblem/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        int maxValue;
+        Tuple<int, int>[] subset;
+        KnapsackProblem(
+            new Tuple<int, int>[] {
+                new Tuple<int, int>(5, 5),
+                new Tuple<int, int>(9, 2),
+                new Tuple<int, int>(3, 7),
+                new Tuple<int, int>(6, 1),
+                new Tuple<int, int>(1, 2)
+            },
+            5,
+            out maxValue,
+            out subset);
+        Console.WriteLine($"maxValue: {maxValue}");
+        Console.WriteLine($"subset:");
+        subset.ToList().ForEach(p => { Console.Write($"{p}, "); });
+        Console.WriteLine();
+    }
+
+    // weightLimit is a positive int
+    // pairs are value-weight pairs where all values and weights are positive ints
+    // outputs subset which is a subset of pairs with maximum total value and total weight <= weightLimit
+    // outputs maxValue which is the total value of subset
+    public static void KnapsackProblem(Tuple<int, int>[] pairs, int weightLimit, out int maxValue, out Tuple<int, int>[] subset) {
+        var table = new int[pairs.Length + 1, weightLimit + 1];
+        for (int i = 1; i <= pairs.Length; i++) {
+            for (int wl = 1; wl <= weightLimit; wl++) {
+                var currentPair = pairs[i-1];
+                var currentValue = currentPair.Item1;
+                var currentWeight = currentPair.Item2;
+                if (currentWeight > wl) {
+                    table[i, wl] = table[i-1, wl];
+                } else { // currentWeight <= wl
+                    table[i, wl] = Math.Max(
+                        table[i-1, Math.Max(wl-currentWeight, 0)] + currentValue,
+                        table[i-1, currentWeight]
+                    );
+                }
+            }
+        }
+        maxValue = table[pairs.Length, weightLimit];
+
+        // now lets figure out the subset
+        var subsetList = new List<Tuple<int,int>>(pairs.Length);
+        var i1 = pairs.Length;
+        var wl1 = weightLimit;
+        while (i1 != 0 && wl1 > 0) {
+            var current = table[i1, wl1];
+            var currentPair = pairs[i1 - 1];
+            var currentValue = currentPair.Item1;
+            var currentWeight = currentPair.Item2;
+            if (currentWeight > wl1) {
+                i1 = i1 - 1;
+            } else { // currentWeight <= wl
+                var valueIncludingCurrentPair = table[i1-1, wl1 - currentWeight] + currentValue;
+                var valueWithoutCurrentPair = table[i1-1, wl1];
+                if (valueIncludingCurrentPair > valueWithoutCurrentPair) {
+                    subsetList.Add(currentPair);
+                    i1 = i1-1;
+                    wl1 = wl1 - currentWeight;
+                } else {
+                    i1 = i1-1;
+                }
+            }
+        }
+        subset = subsetList.ToArray();
+    }
+}
--- subsetSum/Program.cs
+++ subsetSum/Program.cs
@@ -1,21 +1,62 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 public class Program
 {
     public static void Main(String[] args)
     {
-        int maxSubsetWeight;
+        int maxSubsetSum;
         int[] subset;
-        SubsetSum(21, new int[] {5, 10, 12, 7, 15, 13, 8, 25}, out maxSubsetWeight, out subset);
-        Console.WriteLine($"maxSubsetWeight: {maxSubsetWeight}");
+        SubsetSum(new int[] {5, 10, 12, 7, 15, 13, 7, 25}, 21, out maxSubsetSum, out subset);
+        Console.WriteLine($"maxSubsetSum: {maxSubsetSum}");
         Console.WriteLine($"subset:");
-        subset.ToList().ForEach(n => { Console.Write(n); });
+        subset.ToList().ForEach(n => { Console.Write($"{n}, "); });
         Console.WriteLine();
     }
+    // sumLimit is a positive int
+    // nums are all positive ints
+    // outputs subset which is a subset of nums with maximum sum <= sumLimit
+    // outputs maxSubsetSum which is the sum of subset
+    public static void SubsetSum(int[] nums, int sumLimit, out int maxSubsetSum, out int[] subset) {
+        var table = new int[nums.Length + 1, sumLimit + 1];
+        for (int numNums = 1; numNums <= nums.Length; numNums++) {
+            for (int maxSum = 1; maxSum <= sumLimit; maxSum++) {
+                var lastNum = nums[numNums-1];
+                if (lastNum > maxSum) {
+                    // lastNum definitely not in subset
+                    table[numNums, maxSum] = table[numNums-1, maxSum];
+                } else { // lastNum <= maxSum
+                    table[numNums, maxSum] = Math.Max(
+                        table[numNums-1, Math.Max(maxSum-lastNum, 0)] + lastNum,
+                        table[numNums-1, maxSum]
+                    );
+                }
+            }
+        }
+        maxSubsetSum = table[nums.Length, sumLimit];
 
-    public static void SubsetSum(int weightLimit, int[] nums, out int maxSubsetWeight, out int[] subset) {
-        maxSubsetWeight = 0;
-        subset = new int[] { 0 };
+        // now lets figure out the subset
+        var subsetList = new List<int>(nums.Length);
+        var numNums1 = nums.Length;
+        var maxSum1 = sumLimit;
+        while (numNums1 != 0 && maxSum1 > 0) {
+            var current = table[numNums1, maxSum1];
+            var lastNum = nums[numNums1 - 1];
+            if (lastNum > maxSum1) {
+                numNums1 = numNums1 - 1;
+            } else { // lastNum <= maxSum1
+                var sumIncludingLastNum = table[numNums1-1, maxSum1 - lastNum] + lastNum;
+                var sumWithoutLastNum = table[numNums1-1, maxSum1];
+                if (sumIncludingLastNum > sumWithoutLastNum) {
+                    subsetList.Add(lastNum);
+                    numNums1 = numNums1-1;
+                    maxSum1 = maxSum1 - lastNum;
+                } else {
+                    numNums1 = numNums1-1;
+                }
+            }
+        }
+        subset = subsetList.ToArray();
     }
 }
